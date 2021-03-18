07242f81b0a8ea572183ec70e6dfa6186239bd12
WildCodeSchool <anelisynagano@gmail.com>
2021-03-18 12:48:36 +0000

    finish exercise 2

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const body = document.body;
+body.style.backgroundImage =
+  "url('https://www.transparenttextures.com/patterns/argyle.png')";
 
-body.style.backgroundImage = "url('https://www.transparenttextures.com/patterns/argyle.png')"
+const replaceText = document.getElementById("replace-text");
+replaceText.style.color = "#777";
+replaceText.style.fontSize = "2em";
+replaceText.innerHTML = "I can do <em>whatever</em> I want with JavaScript."

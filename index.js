f53639f8da80d9cf38475e6eb8a9b457a6e8f92d
WildCodeSchool <anelisynagano@gmail.com>
2021-03-18 12:58:27 +0000

    finish exercise 3

--- index.js
+++ index.js
@@ -5,4 +5,10 @@ body.style.backgroundImage =
 const replaceText = document.getElementById("replace-text");
 replaceText.style.color = "#777";
 replaceText.style.fontSize = "2em";
-replaceText.innerHTML = "I can do <em>whatever</em> I want with JavaScript."
+replaceText.innerHTML = "I can do <em>whatever</em> I want with JavaScript.";
+
+const blueElements = document.getElementsByClassName("blue");
+
+for (let i = 0; i < blueElements.length; i++) {
+  blueElements[i].style.color = "blue";
+}
